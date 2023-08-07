97bd524eb05afda84101e3f04ec34cb29e67f204
Christian <christianruslim9902@gmail.com>
2023-08-07 15:37:12 +0700

    client database download

--- src/screens/Clients/SSClientScreen.js
+++ src/screens/Clients/SSClientScreen.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 import MenuImage from "../../components/MenuImage/MenuImage";
+import * as Print from 'expo-print';
+import { shareAsync } from 'expo-sharing';
 
 export default function SSClientScreen({ navigation, route }) {
   const { clientDatabase } = route.params;
@@ -9,9 +11,11 @@ export default function SSClientScreen({ navigation, route }) {
   const [data, setData] = useState([]);
   const [tableHead, setTableHead] = useState([]);
   const [widthArr, setWidthArr] = useState([]);
+  const [html, setHTML] = useState('');
 
   const [activeColumn, setActiveColumn] = useState('NamaClient');
   const [isSort, setIsSort] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   const elementButton = (value) => (
     <TouchableOpacity style={styles.btnContainer} onPress={() => handleColumnPress(value)}>
@@ -34,18 +38,30 @@ export default function SSClientScreen({ navigation, route }) {
   };
 
   const fields = [
-    { key: 'Ref', title: '#ID', width: 150 },
+    { key: 'Ref', title: '#ID', width: 175 },
     { key: 'NamaClient', title: 'Nama Client', width: 150 },
     { key: 'NamaPT', title: 'PT Client', width: 150 },
-    { key: 'Job', title: 'Position', width: 100 },
+    { key: 'Job', title: 'Position', width: 150 },
     { key: 'Alamat', title: 'Alamat', width: 100 },
     { key: 'NoTelp', title: 'Telp', width: 100 },
     { key: 'Email', title: 'Email', width: 150 },
     { key: 'Progress', title: 'Progress', width: 100 },
     { key: 'Quo', title: 'Quo Submitted', width: 50 },
-    { key: 'Since', title: 'Since', width: 125 },
     { key: 'By', title: 'Via', width: 100 },
+    { key: 'Since', title: 'Since', width: 125 },
   ];
+
+  let generatePdf = async () => {
+    if (html === '') {
+      setIsEmpty(true);
+      return;
+    }
+    const { uri } = await Print.printToFileAsync({
+      html
+    });
+
+    await shareAsync(uri, { UTI: '.pdf', mimeType: 'application/pdf' });
+  };
 
   useEffect(() => {
     navigation.setOptions({
@@ -64,13 +80,47 @@ export default function SSClientScreen({ navigation, route }) {
           <TouchableOpacity onPress={() => setIsSort(!isSort)}>
             <Image style={styles.filterIcon} source={isSort ? require('../../../assets/icons/ascending.png') : require('../../../assets/icons/descending.png')} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => {}}>
+          <TouchableOpacity onPress={() => {generatePdf()}}>
             <Image style={styles.filterIcon} source={require('../../../assets/icons/download.png')} />
           </TouchableOpacity>
         </View>
       ),
     });
   }, [isSort]);
+
+  useEffect(() => {
+    setHTML(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <style>
+          @page {
+            size: A4;
+          }
+
+          th {
+            background-color: #dddddd;
+          }
+          
+          tr:nth-child(even) {
+            background-color: #dddddd;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Clients Database</h1>
+        <table>
+          <thead>
+            <tr>${fields.map((head) => `<th>${head.title}</th>`).join('')}</tr>
+          </thead>
+          <tbody>
+            ${data.map((row) => `<tr>${row.map((cell) => `<td>${cell}</td>`).join('')}</tr>`).join('')}
+          </tbody>
+        </table>
+      </body>
+    </html>
+    `);
+  }, [data]);
 
   useEffect(() => {
     const rows = clientDatabase.slice().sort((a, b) => {
@@ -100,7 +150,12 @@ export default function SSClientScreen({ navigation, route }) {
         });
       })
     );
-  }, [clientDatabase, isSort, activeColumn]);
+
+    if (isEmpty) {
+      generatePdf();
+      setIsEmpty(false);
+    }
+  }, [clientDatabase, isSort, activeColumn, isEmpty]);
 
   return (
     <View style={styles.container}>
